e36390bb22351afa6f58861bde8eb9617133218e
aoavzm <aoavzm@gmail.com>
2022-08-09 21:07:32 -0700

    setup 1

---
+++ backend/commands.sh
@@ -0,0 +1,7 @@
+npm install cookie-parser cors csurf dotenv express express-async-errors helmet jsonwebtoken morgan per-env sequelize@6 sequelize-cli@6
+
+npm install -D sqlite3 dotenv-cli nodemon
+
+npx sequelize-cli init
+
+openssl rand -base64 10
